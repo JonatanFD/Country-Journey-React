1d5b41fa5d6d67787963d825bf9f36ed636eb2f8
JonatanFD <acunacorahuajon@gmail.com>
2024-10-11 20:57:12 -0500

    form added

--- src/components/JourneyForm.tsx
+++ src/components/JourneyForm.tsx
@@ -1,21 +1,71 @@
+import { z } from "zod";
+import { Button } from "./ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "./ui/input";
+
+
+const formSchema = z.object({
+  from: z.string({message: "El origen es requerido"}),
+  to: z.string({message: "El destino es requerido"}),
+  filters: z.string().optional(),
+})
 
 export default function JourneyForm() {
-  return (
-    <section className="absolute top-4 left-4">
-        <form>
-            <label htmlFor="from">Origen:</label>
-            <input type="text" id="from" name="from" />
 
-            <label htmlFor="to">Destino:</label>
-            <input type="text" id="to" name="to" />
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  })
 
-            <label htmlFor="filters">Países:</label>
-            <input type="text" id="filters" name="filters" />
-
-            <button type="submit">Search</button>
-        </form>
+  const onSubmit = form.handleSubmit((data) => {
+    console.log(data);
+  })
 
 
-    </section>
-  )
+    return (
+        <section className="absolute top-4 left-4 z-30">
+            <Form {...form}>
+                <form onSubmit={onSubmit} className="space-y-8">
+                    <FormField
+                        control={form.control}
+                        name="from"
+                        render={({ field }) => (
+                            <FormItem>
+                              <FormLabel>
+                                Origen
+                              </FormLabel>
+
+                              <FormControl>
+                                <Input {...field} />
+                              </FormControl>
+
+                              <FormMessage />
+                            </FormItem>
+                        )}
+                     />
+
+                    <FormField
+                        control={form.control}
+                        name="to"
+                        render={({ field }) => (
+                            <FormItem>
+                              <FormLabel>
+                                Destino
+                              </FormLabel>
+
+                              <FormControl>
+                                <Input {...field} />
+                              </FormControl>
+
+                              <FormMessage />
+                            </FormItem>
+                        )}
+                      />
+
+                    <Button className="w-full">Buscar</Button>
+                </form>
+            </Form>
+        </section>
+    );
 }
